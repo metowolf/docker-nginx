1cd92da0e5e1dac8744677ddcfaf20e43a54f18e
metowolf <i@i-meto.com>
2019-04-10 17:01:02 +0800

    update travis build

--- build.sh
+++ build.sh
@@ -2,13 +2,37 @@
 
 set -e
 
-VERSION=`cat Dockerfile | grep 'ARG NGINX_VERSION' | awk -F '=' '{print $2}'`
+VERSION=`cat Dockerfile | grep ${VERSION_KEYWORD} | awk -F '=' '{print $2}'`
+MAJOR=`echo ${VERSION} | cut -d. -f1`
+MINOR=`echo ${VERSION} | cut -d. -f2`
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t metowolf/nginx .
-
+docker build --no-cache -t ${IMAGE_NAME}:travis .
 docker images
 
-docker push metowolf/nginx
-docker tag metowolf/nginx metowolf/nginx:$VERSION
-docker push metowolf/nginx:$VERSION
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+
+  if [ "$TRAVIS_BRANCH" = "master" ]; then
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:latest
+    docker push ${IMAGE_NAME}:latest
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:${MAJOR}
+    docker push ${IMAGE_NAME}:${MAJOR}
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:${MAJOR}.${MINOR}
+    docker push ${IMAGE_NAME}:${MAJOR}.${MINOR}
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:${VERSION}
+    docker push ${IMAGE_NAME}:${VERSION}
+
+  else
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:dev
+    docker push ${IMAGE_NAME}:dev
+
+    docker tag ${IMAGE_NAME}:travis ${IMAGE_NAME}:${VERSION}-dev
+    docker push ${IMAGE_NAME}:${VERSION}-dev
+
+  fi
+
+fi
